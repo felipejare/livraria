9e3b0753bee94abcbd418816fe4d8cc921452bc5
felipejare <felipeakf@gmail.com>
2023-03-21 16:01:45 -0300

    Adição de uma model de Livros

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
-from livraria.models import Autor, Categoria, Editora
+from livraria.models import Autor, Categoria, Editora, Livro
 
 admin.site.register(Categoria)
-
 admin.site.register(Editora)
-
 admin.site.register(Autor)
+admin.site.register(Livro)
