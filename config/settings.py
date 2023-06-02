9ebeacb5a2c6502ff32710b3e1945547e24f156b
felipejare <felipeakf@gmail.com>
2023-06-02 16:58:31 -0300

    Adição de CORS Headers

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'livraria',
 ]
 
@@ -46,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -123,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from rest_framework.routers import DefaultRouter
 from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
-router.register(r"editora", EditoraViewSet)
+router.register(r"editoras", EditoraViewSet)
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"autores", AutorViewSet)
 router.register(r"livros", LivroViewSet)
