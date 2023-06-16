446a1423296e0764037b96508c0309c045fb48f2
felipejare <felipeakf@gmail.com>
2023-06-16 16:59:34 -0300

    Adição de usuarios e permissões

--- livraria/views.py
+++ livraria/views.py
@@ -15,10 +15,13 @@ from livraria.serializers import (
     
 )
 
+# from rest_framework.permissions import IsAuthenticated
+
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    # permission_classes = [IsAuthenticated]
 
 
 class EditoraViewSet(ModelViewSet):
