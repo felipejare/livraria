dd63ac10cfb0c2be0b1ca70bb087067c1d49350c
felipejare <felipeakf@gmail.com>
2023-08-11 10:25:08 -0300

    24# - Criação da entidade compra integrada ao usuário

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from livraria.models import Autor, Categoria, Editora, Livro
+from livraria.models import Autor, Categoria, Editora, Livro, Compra
+
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
 admin.site.register(Livro)
+admin.site.register(Compra)
